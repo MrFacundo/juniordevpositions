fa3268d2fb37d4e06807a0c4015410c09a032ef0
Facundo Troitero <ftroitero@gmail.com>
2020-09-03 13:32:22 +0200

    deploy changes

---
+++ public/precache-manifest.c69324b44ce9dacbccc8c6224f3e44ce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b79519195b539f6e0990f5ca9a39557b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e0d75cd9801aa5675df1",
+    "url": "/static/css/main.bc38b30e.chunk.css"
+  },
+  {
+    "revision": "27823f87657f3ee1e80f",
+    "url": "/static/js/2.85f1f3bc.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.85f1f3bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e0d75cd9801aa5675df1",
+    "url": "/static/js/main.ea27df8f.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
